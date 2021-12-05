f861469a48899d223ec86e6a20d22b91480b867e
Varik Valefor <varikvalefor@aol.com>
2021-12-04 23:14:13 -0500

    soundAlarm is fleshed out a bit.

--- app/albertlertd.hs
+++ app/albertlertd.hs
@@ -41,4 +41,21 @@ nabSystemInfo = error "This thing is unimplemented.";
 -- | @soundAlarm k@ sounds some alarms iff @k@ indicates that something
 -- goes wrong.
 soundAlarm :: SystemInfo -> IO ();
-soundAlarm = error "This thing is unimplemented.";
+soundAlarm k
+  | temperature k > 350 = soundThermalAlarm >>
+                          soundAlarm k {temperature = 0}
+  | systemIsOverheating = soundTheBatSignal >>
+                          soundAlarm k {currBatVoltage = ratedBatVoltage k}
+  | otherwise = return ()
+  where
+  systemIsOverheating :: Bool
+  systemIsOverheating = currBatVoltage k / ratedBatVoltage k < 0.75
+  --
+  soundThermalAlarm = playAudioFile "OVERHEAT.WAV"
+  soundTheBatSignal = playAudioFile "BATSIGNL.WAV";
+
+-- | @playAudioFile k@ plays the audio file whose name is @k@.
+playAudioFile :: String
+              -- ^ The name of the audio file which is to be played
+              -> IO ();
+playAudioFile = error "This thing is unimplemented.";
