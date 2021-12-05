a34766d3af538cee42a41fbcb0dd123f868bb331
Varik Valefor <varikvalefor@aol.com>
2021-12-05 13:35:53 -0500

    s/ $//

--- app/albertlertd.hs
+++ app/albertlertd.hs
@@ -64,7 +64,7 @@ batteryIsUnderVolted k
   | isNothing $ currBatVoltage k = False
   | otherwise = cV / rV < 0.75
   where
-  cV = fromJust (currBatVoltage k) 
+  cV = fromJust (currBatVoltage k)
   rV = fromJust (ratedBatVoltage k);
 
 -- | @playAudioFile k@ plays the audio file whose path is
